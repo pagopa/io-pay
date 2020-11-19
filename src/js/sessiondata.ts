09a12ae762299d7204ac93b30ca43c09866d741e
Vito Falco <vitofalco@MacBook-Pro-di-Vito.local>
2020-11-19 10:32:57 +0100

    Init project

---
+++ src/js/sessiondata.ts
@@ -0,0 +1,33 @@
+import { getUrlParameter } from './urlUtilities';
+import userSessionData from '../assets/json/userSession.json';
+
+export function userSession() {
+    document.body.classList.add("loading");
+
+    const paymentIDStored = sessionStorage.getItem("paymentID");
+    const paymentByQS = getUrlParameter("p")!='' ? getUrlParameter("p") : null;
+    const paymentID = paymentIDStored!=null ? paymentIDStored : paymentByQS;
+    if (paymentID==null) return false;
+
+    const amountPrettified = parseInt(userSessionData.data.payment.amount.amount) / 100;
+    sessionStorage.setItem("amount", amountPrettified);
+    
+    //FAKE IMPLEMENTATION
+    const enteBeneficiario = document.querySelector("[data-sessiondata='enteBeneficiario']");
+    const subject = document.querySelector("[data-sessiondata='subject']");
+    const importo = document.querySelector("[data-sessiondata='importo']");
+
+    const importoValue = userSessionData.data.payment.detailsList[0].importo;
+
+    enteBeneficiario.innerText = userSessionData.data.payment.detailsList[0].enteBeneficiario;
+    subject.innerText = userSessionData.data.payment.subject;
+    importo.innerText = `€ ${Intl.NumberFormat('it-IT').format(importoValue)}`;
+
+    
+    
+    if (paymentIDStored==null) sessionStorage.setItem("paymentID", paymentID);
+
+    document.body.classList.remove("loading");
+    
+
+}
