190d0ee42c22691e2d64cb50ae060fcfa9396af2
Giovanni Mancini <g.mancini.0gmail.com>
2020-11-26 19:14:20 +0100

    [#175807613] Fixed many error by hand trying to preserve code logic and structure

--- src/js/urlUtilities.ts
+++ src/js/urlUtilities.ts
@@ -1,6 +1,6 @@
-export function getUrlParameter(name) {
-    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-    const regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+export function getUrlParameter(name: string) {
+    const myname = name.replace(/[[]/, '\\[').replace(/[\]]/, '\\]');
+    const regex = new RegExp('[\\?&]' + myname + '=([^&#]*)');
     const results = regex.exec(location.search);
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 }
