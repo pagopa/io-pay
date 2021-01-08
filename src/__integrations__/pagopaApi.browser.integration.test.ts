69269082411f9855f1807574e18bcaeba813cfec
Giovanni Mancini <g.mancini.0gmail.com>
2021-01-08 11:57:56 +0100

    [#176339507] Added teardown of the browser object

--- src/__integrations__/pagopaApi.browser.integration.test.ts
+++ src/__integrations__/pagopaApi.browser.integration.test.ts
@@ -1,21 +1,21 @@
-import { debug as cdebug } from 'console';
+// import { debug as cdebug } from 'console';
 import { Browser, launch } from 'puppeteer';
 
 describe('Hover over image test:', () => {
   it('weak test', async () => {
     const myBrowser: Browser = await launch();
     const page = await myBrowser.newPage();
-    const google = await page.goto('https://www.google.com');
-    cdebug(google);
     const result = await page.evaluate(() =>
       fetch('https://api.ratesapi.io/api/latest', {
         headers: {},
         method: 'GET',
       }).then(x => x.json()),
     );
-    cdebug(result);
+
+    expect(result.base).toEqual('EUR');
+    await myBrowser.close();
   });
   /* afterEach(async () => {
-    await browser.close();
+    
   }); */
 });
