107f48e83f08e7bd1d5b367485e856056f769f1b
Giovanni Mancini <g.mancini.0gmail.com>
2021-01-25 09:42:04 +0100

    [#176536899] Added parcel-bundler to dev server. Added test cases for Visa and Mastercard

--- src/__integrations__/creditCard.devServer.integration.test.ts
+++ src/__integrations__/creditCard.devServer.integration.test.ts
@@ -1,25 +1,77 @@
-import { debug } from 'console';
 import { Server } from 'http';
 
 import { Browser, launch } from 'puppeteer';
 import { createHttpTerminator } from 'http-terminator';
 import devServer from './devServer';
 
-describe('Custom Fetches test in browser environment', () => {
+describe('Credit Card Field', () => {
   const PORT = 5000;
   const HOST = 'localhost';
 
-  it('When the user navigate to the bad endpoint of the local server, it should respond 404', async () => {
+  it('should show a warning text when the credit card number is wrong', async () => {
     // Start server
     const myDevServer: Server = devServer.listen(PORT, HOST);
     const devServerTerminator = createHttpTerminator({ server: myDevServer });
 
     // Start the browser environment
-    const myBrowser: Browser = await launch();
+    const myBrowser: Browser = await launch({ headless: true });
     const page = await myBrowser.newPage();
 
-    const serverResponse = await page.goto(`http://${HOST}:${PORT}/?p=1234`);
-    debug(await serverResponse?.text());
+    await page.goto(`http://${HOST}:${PORT}/index.html?p=1234`);
+    await page.setViewport({ width: 1200, height: 907 });
+
+    const creditCardFieldS = '#creditcardnumber';
+    await page.focus(creditCardFieldS);
+    await page.keyboard.type('2324234342423');
+
+    const errorMsgS = '.creditform > #creditcardform > .form-group > .is-invalid > .custom-label--error';
+
+    const errorMsgV = await page.$eval(errorMsgS, element => element.textContent);
+
+    expect(errorMsgV).toMatch(/Inserisci un numero valido/);
+    await Promise.all([devServerTerminator.terminate(), myBrowser.close()]);
+  });
+
+  it('should handle Mastercard cards', async () => {
+    // Start server
+    const myDevServer: Server = devServer.listen(PORT, HOST);
+    const devServerTerminator = createHttpTerminator({ server: myDevServer });
+
+    // Start the browser environment
+    const myBrowser: Browser = await launch({ headless: false });
+    const page = await myBrowser.newPage();
+
+    await page.goto(`http://${HOST}:${PORT}/index.html?p=1234`);
+    await page.setViewport({ width: 1200, height: 907 });
+
+    const creditCardFieldS = '#creditcardnumber';
+
+    await page.focus(creditCardFieldS);
+    await page.keyboard.type('5555555555554444');
+    const checked = await page.$eval(creditCardFieldS, element => element.getAttribute('data-checked'));
+    expect(checked).toMatch(/1/);
+
+    await Promise.all([devServerTerminator.terminate(), myBrowser.close()]);
+  });
+
+  it('should handle Visa cards', async () => {
+    // Start server
+    const myDevServer: Server = devServer.listen(PORT, HOST);
+    const devServerTerminator = createHttpTerminator({ server: myDevServer });
+
+    // Start the browser environment
+    const myBrowser: Browser = await launch({ headless: false });
+    const page = await myBrowser.newPage();
+
+    await page.goto(`http://${HOST}:${PORT}/index.html?p=1234`);
+    await page.setViewport({ width: 1200, height: 907 });
+
+    const creditCardFieldS = '#creditcardnumber';
+
+    await page.focus(creditCardFieldS);
+    await page.keyboard.type('4111111111111111');
+    const checked = await page.$eval(creditCardFieldS, element => element.getAttribute('data-checked'));
+    expect(checked).toMatch(/1/);
 
     await Promise.all([devServerTerminator.terminate(), myBrowser.close()]);
   });
--- src/__integrations__/devServer.ts
+++ src/__integrations__/devServer.ts
@@ -1,28 +1,11 @@
-import { readFileSync } from 'fs';
-import path from 'path';
 import express from 'express';
+import Bundler from 'parcel-bundler';
 
 const app = express();
-const entry = readFileSync(path.resolve(__dirname, '..', 'dist/index.html'), 'utf8');
 
-app.get('/api/healthcheck', (_, res) => res.send('Healthy!'));
-
-app.use('/', (_, res) => res.send(entry));
-
-// dev-server.js
-/*
-const Bundler = require('parcel-bundler');
-
-// const entry = path.resolve('src/index.html');
-const bundle = new Bundler(entry);
+const entry = 'src/*.pug';
+const bundle = new Bundler(entry, {});
 
 app.use(bundle.middleware());
-
-app.listen(process.env.NODE_ENV, err => {
-  if (err) throw err;
-
-  console.log(`Listening at http://localhost:${process.env.PORT}`);
-});
-*/
 
 export default app;
