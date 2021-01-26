66052cc56202b944fbb852303ff6da80fbcab7f4
Giovanni Mancini <g.mancini.0gmail.com>
2021-01-26 16:17:26 +0100

    [#176536899] Added build stage to test:coverage target. Factored out common code from test cases

--- src/__integrations__/creditCard.devServer.integration.test.ts
+++ src/__integrations__/creditCard.devServer.integration.test.ts
@@ -1,21 +1,45 @@
-// import { debug } from 'console';
 import { Server } from 'http';
 
 import { Browser, launch } from 'puppeteer';
-import { createHttpTerminator } from 'http-terminator';
+import { createHttpTerminator, HttpTerminator } from 'http-terminator';
 import devServer from './devServer';
 
 describe('Credit Card Field', () => {
   const PORT = 40000;
   const HOST = 'localhost';
 
-  it('should show a warning text when the credit card number is wrong', async () => {
-    // Start server
-    const myDevServer: Server = devServer.listen(PORT, HOST);
-    const devServerTerminator = createHttpTerminator({ server: myDevServer });
+  // eslint-disable-next-line functional/no-let
+  let myDevServer: Server;
+  // eslint-disable-next-line functional/no-let
+  let devServerTerminator: HttpTerminator;
+  // eslint-disable-next-line functional/no-let
+  let myBrowser: Browser;
 
+  beforeAll(() => {
+    // Start server
+    myDevServer = devServer.listen(PORT, HOST);
+    devServerTerminator = createHttpTerminator({ server: myDevServer });
+  });
+
+  afterAll(async () => {
+    await devServerTerminator.terminate();
+  });
+
+  beforeEach(async () => {
+    myBrowser = await launch({ headless: true });
+    // Health check
+    const page = await myBrowser.newPage();
+    const serverResponse = await page.goto(`http://${HOST}:${PORT}/health-check`);
+    expect(serverResponse?.status()).toEqual(200);
+    await page.close();
+  });
+
+  afterEach(async () => {
+    await myBrowser.close();
+  });
+
+  it('should show a warning text when the credit card number is wrong', async () => {
     // Start the browser environment
-    const myBrowser: Browser = await launch({ headless: true });
     const page = await myBrowser.newPage();
 
     await page.goto(`http://${HOST}:${PORT}/index.html?p=1234`);
@@ -30,16 +54,10 @@ describe('Credit Card Field', () => {
     const errorMsgV = await page.$eval(errorMsgS, element => element.textContent);
 
     expect(errorMsgV).toMatch(/Inserisci un numero valido/);
-    await Promise.all([devServerTerminator.terminate(), myBrowser.close()]);
+    await page.close();
   });
 
   it('should handle Mastercard cards', async () => {
-    // Start server
-    const myDevServer: Server = devServer.listen(PORT, HOST);
-    const devServerTerminator = createHttpTerminator({ server: myDevServer });
-
-    // Start the browser environment
-    const myBrowser: Browser = await launch({ headless: true });
     const page = await myBrowser.newPage();
 
     await page.goto(`http://${HOST}:${PORT}/index.html?p=1234`);
@@ -51,17 +69,9 @@ describe('Credit Card Field', () => {
     await page.keyboard.type('5555555555554444');
     const checked = await page.$eval(creditCardFieldS, element => element.getAttribute('data-checked'));
     expect(checked).toMatch(/1/);
-
-    await Promise.all([devServerTerminator.terminate(), myBrowser.close()]);
   });
 
   it('should handle Visa cards', async () => {
-    // Start server
-    const myDevServer: Server = devServer.listen(PORT, HOST);
-    const devServerTerminator = createHttpTerminator({ server: myDevServer });
-
-    // Start the browser environment
-    const myBrowser: Browser = await launch({ headless: true });
     const page = await myBrowser.newPage();
 
     await page.goto(`http://${HOST}:${PORT}/index.html?p=1234`);
@@ -73,17 +83,11 @@ describe('Credit Card Field', () => {
     await page.keyboard.type('4111111111111111');
     const checked = await page.$eval(creditCardFieldS, element => element.getAttribute('data-checked'));
     expect(checked).toMatch(/1/);
-
-    await Promise.all([devServerTerminator.terminate(), myBrowser.close()]);
+    await page.close();
   });
 
   it('should handle Maestro cards', async () => {
-    // Start server
-    const myDevServer: Server = devServer.listen(PORT, HOST);
-    const devServerTerminator = createHttpTerminator({ server: myDevServer });
-
     // Start the browser environment
-    const myBrowser: Browser = await launch({ headless: true });
     const page = await myBrowser.newPage();
 
     await page.goto(`http://${HOST}:${PORT}/index.html?p=1234`);
@@ -103,17 +107,10 @@ describe('Credit Card Field', () => {
     // Different length
     await page.keyboard.type('6799990100000000019');
     await expect(page.$eval(creditCardFieldS, element => element.getAttribute('data-checked'))).resolves.toMatch(/1/);
-
-    await Promise.all([devServerTerminator.terminate(), myBrowser.close()]);
+    await page.close();
   });
 
   it('should handle American Express cards', async () => {
-    // Start server
-    const myDevServer: Server = devServer.listen(PORT, HOST);
-    const devServerTerminator = createHttpTerminator({ server: myDevServer });
-
-    // Start the browser environment
-    const myBrowser: Browser = await launch({ headless: true });
     const page = await myBrowser.newPage();
 
     await page.goto(`http://${HOST}:${PORT}/index.html?p=1234`);
@@ -124,17 +121,10 @@ describe('Credit Card Field', () => {
     await page.focus(creditCardFieldS);
     await page.keyboard.type('340024388482878');
     await expect(page.$eval(creditCardFieldS, element => element.getAttribute('data-checked'))).resolves.toMatch(/1/);
-
-    await Promise.all([devServerTerminator.terminate(), myBrowser.close()]);
+    await page.close();
   });
 
   it('should remove data-checked attribute when the card number is removed', async () => {
-    // Start server
-    const myDevServer: Server = devServer.listen(PORT, HOST);
-    const devServerTerminator = createHttpTerminator({ server: myDevServer });
-
-    // Start the browser environment
-    const myBrowser: Browser = await launch({ headless: true });
     const page = await myBrowser.newPage();
 
     await page.goto(`http://${HOST}:${PORT}/index.html?p=1234`);
@@ -154,6 +144,6 @@ describe('Credit Card Field', () => {
 
     const myAttr = await page.$eval(creditCardFieldS, element => element.getAttribute('data-checked'));
     expect(myAttr).toBeNull();
-    await Promise.all([devServerTerminator.terminate(), myBrowser.close()]);
+    await page.close();
   });
 });
--- src/__integrations__/devServer.ts
+++ src/__integrations__/devServer.ts
@@ -3,4 +3,7 @@ import express from 'express';
 const app = express();
 
 app.use('/', express.static('dist'));
+app.get('/health-check', function (_, res) {
+  res.sendStatus(200);
+});
 export default app;
