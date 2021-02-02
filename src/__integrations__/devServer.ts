6da92ad017b99ffeea0cde2eb329facad68a3bf8
Giovanni Mancini <g.mancini.0gmail.com>
2021-02-02 10:21:14 +0100

    [#175798769] Purged devServer from creditCard tests

--- src/__integrations__/devServer.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-app.use('/', express.static('dist'));
-app.get('/health-check', function (_, res) {
-  res.sendStatus(200);
-});
-export default app;
