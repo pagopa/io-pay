33890ae267edd47a420776ae0b25954996a8d5a3
Giovanni Mancini <g.mancini.0gmail.com>
2021-02-04 09:46:04 +0100

    [#176615471] Added endpoint wallet (POST) to PM Stub

--- src/__integrations__/pm.ts
+++ src/__integrations__/pm.ts
@@ -5,6 +5,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import * as myFake from 'faker/locale/it';
+import { fromNullable } from 'fp-ts/lib/Option';
+import { WalletRequest } from '../../generated/definitions/pagopa/WalletRequest';
 
 // create express server
 const pm: Application = express();
@@ -29,58 +31,78 @@ walletRouter.post('/pp-restapi/v3/users/actions/start-session', function (req, r
   }
 });
 
+walletRouter.post('/pp-restapi/v3/wallet', function (req, res) {
+  WalletRequest.decode(req.body).fold(
+    () => res.sendStatus(500),
+    decodedReq => {
+      if (!req.headers.authorization?.match(/Bearer [\d\w]{128}/)) {
+        return res.sendStatus(401);
+      } else if (
+        // request id {}
+        Object.keys(decodedReq).length === 0
+      ) {
+        return res.sendStatus(500);
+      } else if (decodedReq.data && Object.keys(decodedReq.data).length === 0) {
+        return res.sendStatus(422);
+      } else if (decodedReq.data?.creditCard?.expireYear?.match(/(\d\d.+)|[^\d]+/)) {
+        return res.sendStatus(422);
+      } else if (!Object.prototype.hasOwnProperty.call(decodedReq.data, 'idPagamentoFromEC')) {
+        return res.sendStatus(500);
+      } else {
+        const myPan = fromNullable(decodedReq.data?.creditCard?.pan).getOrElse('4444444444444444');
+        return res.json({
+          data: {
+            idWallet: 40,
+            type: 'CREDIT_CARD',
+            favourite: false,
+            creditCard: {
+              id: 48,
+              holder: decodedReq.data?.creditCard?.holder,
+              pan: '*'.repeat(myPan?.length - 4) + myPan?.substr(myPan.length - 4),
+              expireMonth: decodedReq.data?.creditCard?.expireMonth,
+              expireYear: decodedReq.data?.creditCard?.expireYear,
+              brandLogo: 'http://localhost:8080/wallet/assets/img/creditcard/generic.png',
+              flag3dsVerified: false,
+              brand: 'OTHER',
+              hpan: myFake.random.alphaNumeric(64),
+              onUs: false,
+            },
+            psp: {
+              id: 8,
+              idPsp: 'POSTE1',
+              businessName: 'Poste Italiane',
+              paymentType: 'CP',
+              idIntermediary: 'BANCOPOSTA',
+              idChannel: 'POSTE1',
+              logoPSP: 'http://pagopa-dev:8080/pp-restapi/v3/resources/psp/8',
+              serviceLogo: 'http://pagopa-dev:8080/pp-restapi/v3/resources/service/8',
+              serviceName: 'nomeServizio 02 poste (MOD0)',
+              fixedCost: [Object],
+              appChannel: false,
+              serviceAvailability: 'disponibilitaServizio FRANCESE',
+              urlInfoChannel: 'http://www.test.sia.eu',
+              paymentModel: 0,
+              idCard: 11008,
+              lingua: 'IT',
+              codiceAbi: '06220',
+              isPspOnus: false,
+              solvedByPan: false,
+            },
+            idPsp: 8,
+            pspEditable: false,
+            onboardingChannel: 'WISP',
+            services: ['FA', 'pagoPA', 'BPD'],
+            isPspToIgnore: false,
+            saved: false,
+            registeredNexi: false,
+          },
+        });
+      }
+    },
+  );
+});
+
 const routers: ReadonlyArray<Router> = [walletRouter];
 routers.forEach(r => pm.use(r));
 
 export default pm;
-
-/*  ADD WALLET POST
-
-{
-  data: {
-    idWallet: 40,
-    type: 'CREDIT_CARD',
-    favourite: false,
-    creditCard: {
-      id: 48,
-      holder: 'Ciccio Mio',
-      pan: '************8397',
-      expireMonth: '03',
-      expireYear: '25',
-      brandLogo: 'http://localhost:8080/wallet/assets/img/creditcard/generic.png',
-      flag3dsVerified: false,
-      brand: 'OTHER',
-      hpan: 'eb05fa2d38fba34bc89bb7e92ada279f8424acde82338407e8589fdce0901545',
-      onUs: false
-    },
-    psp: {
-      id: 8,
-      idPsp: 'POSTE1',
-      businessName: 'Poste Italiane',
-      paymentType: 'CP',
-      idIntermediary: 'BANCOPOSTA',
-      idChannel: 'POSTE1',
-      logoPSP: 'http://pagopa-dev:8080/pp-restapi/v3/resources/psp/8',
-      serviceLogo: 'http://pagopa-dev:8080/pp-restapi/v3/resources/service/8',
-      serviceName: 'nomeServizio 02 poste (MOD0)',
-      fixedCost: [Object],
-      appChannel: false,
-      serviceAvailability: 'disponibilitaServizio FRANCESE',
-      urlInfoChannel: 'http://www.test.sia.eu',
-      paymentModel: 0,
-      idCard: 11008,
-      lingua: 'IT',
-      codiceAbi: '06220',
-      isPspOnus: false,
-      solvedByPan: false
-    },
-    idPsp: 8,
-    pspEditable: false,
-    onboardingChannel: 'WISP',
-    services: [ 'FA', 'pagoPA', 'BPD' ],
-    isPspToIgnore: false,
-    saved: false,
-    registeredNexi: false
-  }
-}
-*/
--- src/__integrations__/pmClient.addWallet.integration.test.ts
+++ src/__integrations__/pmClient.addWallet.integration.test.ts
@@ -1,29 +1,28 @@
-// import { debug } from 'console';
-// import { Server } from 'http';
+import { Server } from 'http';
 
 import nodeFetch from 'node-fetch';
 // Needed to patch the globals
 import 'abort-controller/polyfill';
-// import { createHttpTerminator, HttpTerminator } from 'http-terminator';
+import { createHttpTerminator, HttpTerminator } from 'http-terminator';
 import { Millisecond } from 'italia-ts-commons/lib/units';
 import { fromNullable } from 'fp-ts/lib/Option';
 import { createClient } from '../../generated/definitions/pagopa/client';
 import { TypeEnum } from '../../generated/definitions/pagopa/Wallet';
 import { retryingFetch } from '../utils/fetch';
-// import pm from './pm';
+import pm from './pm';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any,functional/immutable-data
 (global as any).fetch = nodeFetch;
 
 describe('Endpoint addWalletUsingPOST', () => {
-  const PORT = process.env.PAYMENT_MANAGER_DOCKER_PORT ? parseInt(process.env.PAYMENT_MANAGER_DOCKER_PORT, 10) : 8080;
-  const HOST = process.env.PAYMENT_MANAGER_DOCKER_HOST as string;
+  const PORT = process.env.PAYMENT_MANAGER_STUB_PORT ? parseInt(process.env.PAYMENT_MANAGER_STUB_PORT, 10) : 8080;
+  const HOST = process.env.PAYMENT_MANAGER_STUB_HOST as string;
 
   // eslint-disable-next-line functional/no-let
-  // let pmServer: Server;
+  let pmServer: Server;
 
   // eslint-disable-next-line functional/no-let
-  // let pmServerTerminator: HttpTerminator;
+  let pmServerTerminator: HttpTerminator;
 
   // eslint-disable-next-line functional/no-let
   let pmClient;
@@ -33,8 +32,8 @@ describe('Endpoint addWalletUsingPOST', () => {
 
   beforeAll(async () => {
     // Start server
-    // pmServer = pm.listen(PORT, HOST);
-    // pmServerTerminator = createHttpTerminator({ server: pmServer });
+    pmServer = pm.listen(PORT, HOST);
+    pmServerTerminator = createHttpTerminator({ server: pmServer });
     // Start client
     pmClient = createClient({
       baseUrl: `http://${HOST}:${PORT}`,
@@ -56,11 +55,11 @@ describe('Endpoint addWalletUsingPOST', () => {
       myRes => fromNullable(myRes.value.data.sessionToken).getOrElse('wrong_token'),
     );
   });
-  /*
+
   afterAll(async () => {
     await pmServerTerminator.terminate();
   });
-*/
+
   it('should return 401 when the session token is wrong', async () => {
     expect(
       (
@@ -125,7 +124,7 @@ describe('Endpoint addWalletUsingPOST', () => {
                 securityCode: '666',
               },
               favourite: true,
-              idPagamentoFromEC: '2a4afce9-40e5-4be7-885b-a5223ff73e9a', // need to exist
+              idPagamentoFromEC: '2a4afce9-40e5-4be7-885b-a5223ff73e9a', // needs to exist
             },
           },
           language: 'it',
@@ -151,7 +150,7 @@ describe('Endpoint addWalletUsingPOST', () => {
                 holder: 'Ciccio Mio',
                 pan: '4024007182788397',
               },
-              idPagamentoFromEC: '2a4afce9-40e5-4be7-885b-a5223ff73e9a', // need to exist
+              idPagamentoFromEC: '', // needs to exist
             },
           },
           language: 'it',
@@ -161,5 +160,31 @@ describe('Endpoint addWalletUsingPOST', () => {
         myRes => myRes.status,
       ),
     ).toEqual(200);
+  });
+
+  it('should return 422 when the credit card expiring year is encoded with 4 digits', async () => {
+    expect(
+      (
+        await pmClient.addWalletUsingPOST({
+          Bearer: 'Bearer ' + sessionToken,
+          walletRequest: {
+            data: {
+              type: TypeEnum.CREDIT_CARD,
+              creditCard: {
+                expireMonth: '03',
+                expireYear: '2025', // year must be encoded with two digits
+                holder: 'Ciccio Mio',
+                pan: '4024007182788397',
+              },
+              idPagamentoFromEC: '2a4afce9-40e5-4be7-885b-a5223ff73e9a', // needs to exist
+            },
+          },
+          language: 'it',
+        })
+      ).fold(
+        err => (err.pop()?.value as Response).status,
+        () => undefined,
+      ),
+    ).toEqual(422);
   });
 });
