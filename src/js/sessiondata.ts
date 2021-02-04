238c9de38508ce0bfbdb9ca8c7af07d67b69ef5a
Giovanni Mancini <g.mancini.0gmail.com>
2021-02-04 19:23:51 +0100

    [#176536665] Fixes tests and local variables

--- src/js/sessiondata.ts
+++ src/js/sessiondata.ts
@@ -1,6 +1,7 @@
 import checkdata from '../assets/json/check.json';
 import idpayguard from './idpayguard';
 import { initHeader } from './header';
+import { getUrlParameter } from './urlUtilities';
 
 export function actionsCheck(): void {
   document.body.classList.add('loading');
@@ -10,6 +11,10 @@ export function actionsCheck(): void {
   // TO-DO: implement call to endpoint
   const checkData = checkdata;
   initHeader(checkData);
+
+  const paymentIDStored: string | null = sessionStorage.getItem('paymentID');
+  const paymentByQS: string | null = getUrlParameter('p') !== '' ? getUrlParameter('p') : null;
+  const paymentID: string | null = paymentIDStored != null ? paymentIDStored : paymentByQS;
 
   if (paymentIDStored == null && paymentID) {
     sessionStorage.setItem('paymentID', paymentID);
