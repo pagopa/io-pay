d817b7f9109a2315760b87c9f8de5ea25d93e5e2
Giovanni Mancini <g.mancini.0gmail.com>
2021-02-11 11:34:53 +0100

    [#175798680] Changed naming convention for docker tests

--- jest.config.integration.docker.js
+++ jest.config.integration.docker.js
@@ -4,7 +4,7 @@ const puppeteerPreset = require('jest-puppeteer/jest-preset');
 module.exports = {
   ...typescriptPreset,
   ...puppeteerPreset,
-  testRegex: '__integrations__/docker/.*docker.integration.test.*',
+  testRegex: '__integrations__/docker/.*integration.docker.test.*',
   testPathIgnorePatterns: ['dist', '/node_modules'],
   setupFiles: ['dotenv/config'],
 };
