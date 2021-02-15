63c84aaaef44218996ff986b5c13772b544978ed
pasqualespica <pasqualespica@gmail.com>
2021-02-15 21:51:30 +0100

    [#176738799] add test resources in to UAT PROD payment manager env

---
+++ src/__integrations__/uat-prod/pmClient.resources.integration.uat-prod.test.ts
@@ -0,0 +1,117 @@
+// import { fromNullable } from 'fp-ts/lib/Option';
+import { Millisecond } from 'italia-ts-commons/lib/units';
+import 'abort-controller/polyfill';
+import nodeFetch from 'node-fetch';
+import { createClient } from '../../../generated/definitions/pagopa/client';
+import { retryingFetch } from '../../utils/fetch';
+import * as myFetch from '../../utils/fetch';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any,functional/immutable-data
+(global as any).fetch = nodeFetch;
+
+describe('Endpoint resources test suite for UAT/PROD PM', () => {
+  // Set the testing environment
+
+  const PM_PORT = process.env.PAYMENT_MANAGER_PORT ? parseInt(process.env.PAYMENT_MANAGER_PORT, 10) : 1234;
+  const PM_HOST_PROD = process.env.PAYMENT_MANAGER_HOST_PROD as string;
+  const PM_HOST_UAT = process.env.PAYMENT_MANAGER_HOST_UAT as string;
+
+  // eslint-disable-next-line functional/no-let
+  let pmClientUAT;
+  // eslint-disable-next-line functional/no-let
+  let pmClientPROD;
+
+  beforeAll(async () => {
+    // Start clients
+
+    pmClientUAT = createClient({
+      baseUrl: `https://${PM_HOST_UAT}:${PM_PORT}`,
+      fetchApi: retryingFetch(fetch, 5000 as Millisecond, 5),
+    });
+
+    pmClientPROD = createClient({
+      baseUrl: `https://${PM_HOST_PROD}:${PM_PORT}`,
+      fetchApi: retryingFetch(fetch, 5000 as Millisecond, 5),
+    });
+  });
+
+  beforeEach(async () => {
+    const mySpyCustomFetch = jest.spyOn(myFetch, 'retryingFetch');
+
+    const paymentManagerClientUAT = createClient({
+      baseUrl: `https://${PM_HOST_UAT}:${PM_PORT}/pp-restapi` as string,
+      fetchApi: retryingFetch(fetch, 5000 as Millisecond, 5),
+    });
+    const paymentManagerClientPROD = createClient({
+      baseUrl: `https://${PM_HOST_PROD}:${PM_PORT}/pp-restapi` as string,
+      fetchApi: retryingFetch(fetch, 5000 as Millisecond, 5),
+    });
+    // Spy on th global fetch to check if it gets called
+    const mySpyGlobalFetch = jest.spyOn(global, 'fetch');
+    expect(paymentManagerClientUAT).toBeTruthy();
+    expect(paymentManagerClientPROD).toBeTruthy();
+    expect(mySpyGlobalFetch).not.toHaveBeenCalled();
+    expect(mySpyCustomFetch).toHaveBeenCalled();
+  });
+
+  it('should return 200 on getResourcesUsingGET response', async () => {
+    expect(
+      (await pmClientUAT.getResourcesUsingGET({ language: 'it' })).fold(
+        err => (err.pop()?.value as Response)?.status,
+        myRes => myRes.status,
+      ),
+    ).toEqual(200);
+
+    expect(
+      (await pmClientPROD.getResourcesUsingGET({ language: 'it' })).fold(
+        err => (err.pop()?.value as Response)?.status,
+        myRes => myRes.status,
+      ),
+    ).toEqual(200);
+  });
+
+  it('should return terms and conditions that shall contains PagoPA S.p.A. ', async () => {
+    expect(
+      (await pmClientUAT.getResourcesUsingGET({ language: 'it' })).fold(
+        err => (err.pop()?.value as Response)?.status,
+        myRes => myRes.value?.data?.termsAndConditions,
+      ),
+    ).toContain('PagoPA S.p.A.');
+    expect(
+      (await pmClientPROD.getResourcesUsingGET({ language: 'it' })).fold(
+        err => (err.pop()?.value as Response)?.status,
+        myRes => myRes.value?.data?.termsAndConditions,
+      ),
+    ).toContain('PagoPA S.p.A.');
+  });
+
+  it('should return terms and conditions that shall NOT contains AgID', async () => {
+    expect(
+      (await pmClientUAT.getResourcesUsingGET({ language: 'it' })).fold(
+        err => (err.pop()?.value as Response)?.status,
+        myRes => myRes.value?.data?.termsAndConditions,
+      ),
+    ).not.toContain('AgID');
+    expect(
+      (await pmClientPROD.getResourcesUsingGET({ language: 'it' })).fold(
+        err => (err.pop()?.value as Response)?.status,
+        myRes => myRes.value?.data?.termsAndConditions,
+      ),
+    ).not.toContain('AgID');
+  });
+
+  it('should return 3DS2 configuration set to FALSE', async () => {
+    expect(
+      (await pmClientUAT.getResourcesUsingGET({ language: 'it' })).fold(
+        err => (err.pop()?.value as Response)?.status,
+        myRes => myRes.value?.data?.is3ds2,
+      ),
+    ).toEqual('false');
+    expect(
+      (await pmClientPROD.getResourcesUsingGET({ language: 'it' })).fold(
+        err => (err.pop()?.value as Response)?.status,
+        myRes => myRes.value?.data?.is3ds2,
+      ),
+    ).toEqual('false');
+  });
+});
