5efa67f351ae8bc643ac9f98f0be91593dcbed4c
pasqualespica <pasqualespica@gmail.com>
2021-02-18 22:26:30 +0100

    [#176942427] add WIP resume transaction docker PM test

---
+++ src/__integrations__/docker/pmClient.resume.integration.docker.test.ts
@@ -0,0 +1,216 @@
+import { Millisecond } from 'italia-ts-commons/lib/units';
+import 'abort-controller/polyfill';
+import nodeFetch from 'node-fetch';
+import { fromNullable } from 'fp-ts/lib/Option';
+import { DeferredPromise } from 'italia-ts-commons/lib/promises';
+// import { identity } from 'fp-ts/lib/function';
+import { createClient, Client } from '../../../generated/definitions/pagopa/client';
+import { constantPollingWithPromisePredicateFetch, retryingFetch } from '../../utils/fetch';
+import { getIdPayment } from '../../utils/testUtils';
+import { TypeEnum } from '../../../generated/definitions/pagopa/Wallet';
+import { WalletResponse } from '../../../generated/definitions/pagopa/WalletResponse';
+import { TransactionStatusResponse } from '../../../generated/definitions/pagopa/TransactionStatusResponse';
+import { fromLeft, taskEither, tryCatch } from 'fp-ts/lib/TaskEither';
+// eslint-disable-next-line @typescript-eslint/no-explicit-any,functional/immutable-data
+(global as any).fetch = nodeFetch;
+
+describe('Endpoint PUT wallet of PM', () => {
+  // Set the testing environment
+
+  const PM_DOCK_PORT = process.env.PAYMENT_MANAGER_DOCKER_PORT
+    ? parseInt(process.env.PAYMENT_MANAGER_DOCKER_PORT, 10)
+    : 1234;
+  const PM_DOCK_HOST = process.env.PAYMENT_MANAGER_DOCKER_HOST as string;
+  const PM_DOCK_CTRL_PORT = process.env.PAYMENT_MANAGER_DOCKER_CONTROL_PORT
+    ? parseInt(process.env.PAYMENT_MANAGER_DOCKER_CONTROL_PORT, 10)
+    : 8081;
+  // eslint-disable-next-line functional/no-let
+  let pmClient: Client;
+  // eslint-disable-next-line functional/no-let
+  let paymentManagerClientWithPolling: Client;
+  // PRECONDITION: Before pay endpoint, execute the following flow
+  // 1. get a valid idPayment
+  // 2. check the payment
+  // 3. start the session
+  // 4. approve terms and conditions
+  // 5. add wallet
+  // 6. pay
+  // 7. trasaction check
+
+  // eslint-disable-next-line functional/no-let
+  let myIdPayment: string;
+  // eslint-disable-next-line functional/no-let
+  let startSessionResponse;
+  // eslint-disable-next-line functional/no-let
+  let walletResponse: WalletResponse;
+  // eslint-disable-next-line functional/no-let
+  let myTransactionToken: string; // base64 string of idTransaction
+  // eslint-disable-next-line functional/no-let
+  let checkTransactionResponseOK: boolean;
+
+  const retries: number = 10;
+  const delay: number = 3000;
+  const timeout: Millisecond = 20000 as Millisecond;
+
+  // This condition is used to retry polling check until it return 200 && finalStatus = true
+  const isTransientErrorGivenFinalStatus = async (response: Response): Promise<boolean> => {
+    const transactionStatus = TransactionStatusResponse.encode(await response.clone().json());
+    return response.status === 200 && transactionStatus.data?.finalStatus === false;
+  };
+
+  beforeAll(async () => {
+    // Start client
+    pmClient = createClient({
+      baseUrl: `http://${PM_DOCK_HOST}:${PM_DOCK_PORT}`,
+      fetchApi: retryingFetch(fetch, 5000 as Millisecond, 5),
+    });
+
+    paymentManagerClientWithPolling = createClient({
+      baseUrl: 'http://localhost:8080',
+      fetchApi: constantPollingWithPromisePredicateFetch(
+        DeferredPromise<boolean>().e1,
+        retries,
+        delay,
+        timeout,
+        isTransientErrorGivenFinalStatus,
+      ),
+    });
+  });
+
+  beforeEach(async () => {
+    // Execute the Happy Path before testing payment
+    myIdPayment = await getIdPayment(PM_DOCK_HOST, PM_DOCK_CTRL_PORT.toString());
+
+    // check
+    (
+      await pmClient.checkPaymentUsingGET({
+        id: myIdPayment,
+      })
+    ).fold(
+      _ => fail(),
+      res => res.value?.data,
+    );
+
+    // start session
+    startSessionResponse = (
+      await pmClient.startSessionUsingPOST({
+        startSessionRequest: {
+          data: {
+            email: 'username@domain.com',
+            fiscalCode: 'UQNSFM56P12T733D',
+            idPayment: myIdPayment,
+          },
+        },
+      })
+    ).fold(
+      _ => fail(),
+      res => res.value,
+    );
+
+    // approve terms
+    (
+      await pmClient.approveTermsUsingPOST({
+        Bearer: `Bearer ${startSessionResponse?.sessionToken}`,
+        approveTermsRequest: {
+          data: {
+            terms: true,
+            privacy: true,
+          },
+        },
+      })
+    ).fold(
+      () => fail(),
+      res => res.value?.data,
+    );
+
+    // POST Wallet
+    walletResponse = (
+      await pmClient.addWalletUsingPOST({
+        Bearer: `Bearer ${startSessionResponse?.sessionToken}`,
+        walletRequest: {
+          data: {
+            type: TypeEnum.CREDIT_CARD,
+            creditCard: {
+              brand: 'VISA',
+              expireMonth: '03',
+              expireYear: '25',
+              holder: 'UserName UserSurname',
+              pan: '4556311820389402',
+              securityCode: '666',
+            },
+            favourite: true,
+            idPagamentoFromEC: myIdPayment,
+          },
+        },
+        language: 'it',
+      })
+    ).fold(
+      () => fail(),
+      res => WalletResponse.decode(res.value).getOrElse({ data: {} }),
+    );
+
+    // Pay
+    const payResponse = (
+      await pmClient.payUsingPOST({
+        Bearer: `Bearer ${startSessionResponse?.sessionToken}`,
+        id: myIdPayment,
+        payRequest: {
+          data: { idWallet: walletResponse?.data?.idWallet, cvv: '666' },
+        },
+        language: 'it',
+      })
+    ).fold(
+      () => fail(),
+      res => res.value?.data,
+    );
+
+    myTransactionToken = fromNullable(payResponse?.token).getOrElse('-1');
+
+    // transaction check
+    const checkTransactionResponse = await tryCatch(
+      () =>
+        paymentManagerClientWithPolling.checkStatusUsingGET({
+          id: myTransactionToken,
+        }),
+      () => -1,
+    )
+      .foldTaskEither(
+        err => fromLeft(err),
+        errorOrResponse =>
+          errorOrResponse.fold(
+            () => fromLeft(-1),
+            responseType => (responseType.status !== 200 ? fromLeft(-1) : taskEither.of(responseType.value)),
+          ),
+      )
+      .run();
+
+    checkTransactionResponseOK = checkTransactionResponse.isRight();
+  });
+
+  it('should return OK 200 response on resume transaction after check transaction response success ', async () => {
+    // precondition
+    expect(checkTransactionResponseOK).toEqual(true);
+
+    // resume
+    const transactionResumeResponse = (
+      await pmClient.resumeUsingPOST({
+        Bearer: `Bearer ${startSessionResponse?.sessionToken}`,
+        id: myTransactionToken,
+        resumeRequest: {
+          data: {},
+        },
+        language: 'it',
+      })
+    ).fold(
+      () => fail(),
+      res => res.status,
+    );
+    expect(transactionResumeResponse).toEqual(200);
+
+    expect(true).toEqual(true);
+  });
+
+  // WIP
+  // add resume3ds2
+  // add errors case scenario
+});
