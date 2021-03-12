1a3e403219378bf71d3da24aae252f373164633a
Vito Falco <vitofalco@gmail.com>
2021-03-12 12:54:07 +0100

    Guard to redirect a user without a idpayment

--- src/js/idpayguard.ts
+++ src/js/idpayguard.ts
@@ -2,12 +2,10 @@ import { getUrlParameter } from './urlUtilities';
 
 // PaymentID: from sessionstorage or by querystring
 export default function idpayguard(): void {
-  const paymentIDStored: string | null = sessionStorage.getItem('paymentID');
+  const paymentIDStored: string | null = sessionStorage.getItem('paymentID') || null;
   const paymentByQS: string | null = getUrlParameter('p') !== '' ? getUrlParameter('p') : null;
-  const paymentID: string | null = paymentIDStored != null ? paymentIDStored : paymentByQS;
-  if (!paymentID) {
-    // TO-DO
-    // eslint-disable-next-line no-console
-    // console.log('TO-DO prevedere exit in caso non ci sia paymentid'); // FIXME:
+  const paymentID: string | null = paymentIDStored !== null ? paymentIDStored : paymentByQS;
+  if (paymentID === null) {
+    window.location.replace('ko.html');
   }
 }
