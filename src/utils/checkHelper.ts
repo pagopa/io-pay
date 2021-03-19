9c8d8bd78ac37a8e7cf1d3c7b883c7530718a3f6
Simone Infante <simone.infante@pagopa.it>
2021-03-19 10:42:53 +0100

    add api to retrive browserInfo

---
+++ src/utils/checkHelper.ts
@@ -0,0 +1,19 @@
+import { fromLeft, taskEither, TaskEither, tryCatch } from 'fp-ts/lib/TaskEither';
+import { BrowserInfoResponse } from '../../generated/definitions/functions/BrowserInfoResponse';
+import { Client } from '../../generated/definitions/iopayportal/client';
+
+export const getBrowserInfoTask = (iopayportalClient: Client): TaskEither<string, BrowserInfoResponse> =>
+  tryCatch(
+    () => iopayportalClient.GetBrowsersInfo({}),
+    () => 'Errore recupero browserInfo',
+  ).foldTaskEither(
+    err => fromLeft(err),
+    errorOrResponse =>
+      errorOrResponse.fold(
+        () => fromLeft('Errore recupero browserInfo'),
+        responseType =>
+          responseType.status !== 200
+            ? fromLeft(`Errore recupero browserInfo : ${responseType.status}`)
+            : taskEither.of(responseType.value),
+      ),
+  );
