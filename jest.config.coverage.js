7e26ab4c42003600f773e4b9b2057db1bc908933
Simone Infante <simone.infante@pagopa.it>
2021-10-13 14:17:26 +0200

    test

--- jest.config.coverage.js
+++ jest.config.coverage.js
@@ -4,7 +4,15 @@ const puppeteerPreset = require('jest-puppeteer/jest-preset');
 module.exports = {
   ...typescriptPreset,
   ...puppeteerPreset,
-  testRegex: '__integrations__/docker/.*integration.docker.test.*',
+  testRegex: '__integrations__.*integration.test.*|__test__.*test.*',
   testPathIgnorePatterns: ['dist', '/node_modules'],
   setupFiles: ['dotenv/config'],
+  reporters: [
+    'default',
+    [ 'jest-junit', {
+      outputDirectory: './test_reports',
+      outputName: 'io-pay-ui-TEST.xml',
+    } ]
+  ],
+  coverageReporters: ["cobertura"],
 };
