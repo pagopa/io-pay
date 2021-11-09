f8659cd31669767ec465073a0c7a95261fff4843
Marco Greco <marcoandreagreco@gmail.com>
2021-11-09 18:00:19 +0100

    [IOPAY-341] fix: change href on cancelled button

--- src/ko.ts
+++ src/ko.ts
@@ -1,11 +1,5 @@
 // eslint-disable-next-line sonarjs/cognitive-complexity
 document.addEventListener('DOMContentLoaded', async () => {
   window.sessionStorage.clear();
-
-  const cancelButton = document.getElementById('cancel');
-  cancelButton?.addEventListener('click', (e: Event) => {
-    e.preventDefault();
-    window.close();
-  });
 });
 export {};
